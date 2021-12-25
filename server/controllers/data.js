8dc5990cf233181b049c1c0ef35f2aeb5ef8580b
Ashwin Rabindran <ar6222@stern.nyu.edu>
2021-12-24 18:10:17 -0600

    harvest crypto pairs from coinbase

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -1,4 +1,51 @@
+import Twit from 'twit';
+import dotenv from 'dotenv'
+import rp from 'request-promise'
+import fs from 'fs'
 export const getData = (req, res) => {
-    res.send("Working");
+    dotenv.config();
+    const cbApi = process.env.cbApi;
+
+
+ 
+    // const requestOptions = {
+    //   method: 'GET',
+    //   uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map',
+    //   qs: {
+ 
+    //   },
+    //   headers: {
+    //     'X-CMC_PRO_API_KEY': cbApi
+    //   },
+    //   json: true,
+    //   gzip: true
+    // };
+    
+    // rp(requestOptions).then(response => {
+    //   console.log('API call response:', response);
+    //   fs.writeFile('cryptoMap.json',  JSON.stringify(response), (error) => {
+    //       if (error) throw error;
+    //   })
+    // }).catch((err) => {
+    //   console.log('API call error:', err.message);
+    // });
+    
+    var T = new Twit({
+        consumer_key:         '...',
+        consumer_secret:      '...',
+        access_token:         '...',
+        access_token_secret:  '...',
+        timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+        strictSSL:            true,     // optional - requires SSL certificates to be valid.
+      })
+
+
+    try {
+        console.log("Data")
+        res.send("Working")
+        
+    } catch (error) {
+        
+    }
 
 }
